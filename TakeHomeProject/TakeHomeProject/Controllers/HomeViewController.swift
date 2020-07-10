c931c7a0df86f9fc30df17b2097984ef6446c131
mohammed <mdrame113@gmail.com>
2020-07-09 19:27:45 -0700

    init vc

---
+++ TakeHomeProject/TakeHomeProject/Controllers/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  TakeHomeProject
+//
+//  Created by Mohammed Drame on 7/9/20.
+//  Copyright © 2020 Mo Drame. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    
+    func addition(first: Int, second: Int)->Int {
+        let result = first + second
+        return result
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+}
+
---
+++ TakeHomeProject/TakeHomeProjectTests/TakeHomeProjectTests.swift
@@ -0,0 +1,42 @@
+//
+//  TakeHomeProjectTests.swift
+//  TakeHomeProjectTests
+//
+//  Created by Mohammed Drame on 7/9/20.
+//  Copyright © 2020 Mo Drame. All rights reserved.
+//
+
+import XCTest
+@testable import TakeHomeProject
+
+class TakeHomeProjectTests: XCTestCase {
+    
+      let vc = HomeViewController()
+   
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testSum() {
+       
+        XCTAssert(vc.addition(first: 10, second: 10) == 20, "Funtion is wrong")
+    }
+
+}
